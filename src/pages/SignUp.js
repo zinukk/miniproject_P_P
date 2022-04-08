88f5a44427107b861e40cf55cffe0033e914a826
ryurim0109 <dbfla5036@naver.com>
2022-04-08 20:52:23 +0900

    파일 생성

---
+++ src/pages/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            hi ㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴ
+        </div>
+    );
+};
+
+export default SignUp;
---
+++ src/pages/Write.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Write = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Write;
+
