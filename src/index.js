ade24a8a6fa4feba529d052004d74614bec425e0
ryurim0109 <dbfla5036@naver.com>
2022-04-09 09:26:45 +0900

    ⚡️ edit store

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './shared/App';
 import reportWebVitals from './reportWebVitals';
 
 import {Provider} from "react-redux";
-import store from "./redux/configStore"
+import store from "./redux/configStore";
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+      <App />
   </Provider>
   ,
   document.getElementById('root')
