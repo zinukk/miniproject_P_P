98d57cead0814d58bc2fa37a71170bf509a3d407
ryurim0109 <dbfla5036@naver.com>
2022-04-11 15:15:24 +0900

    feat.createimage.js

---
+++ src/redux/modules/image.js
@@ -0,0 +1,58 @@
+
+import { createAction,handleActions } from "redux-actions";
+import {produce} from "immer";
+
+
+
+const UPLOADING = "UPLOADING";
+const UPLOAD_IMG ="UPLOAD_IMG";
+const SET_PRE ="SET_PRE";
+
+const uploading =createAction(UPLOADING,(uploading)=>({uploading}));
+const uploadImage =createAction(UPLOAD_IMG,(image_url)=>({image_url}));
+const setPreview =createAction(SET_PRE,(preview)=>({preview}));
+
+const initialState={
+    image_url: '',
+    uploading:false,
+    preview:null,
+}
+
+const uploadImageFB =(image)=>{
+    return function(dispatch,getState,{history}){
+
+        // dispatch(uploading(true));
+        // const _upload =storage.ref( `images/${image.name}`).put(image);
+
+        // _upload.then((snapshot)=>{
+        //     console.log(snapshot);
+        //     snapshot.ref.getDownloadURL().then((url)=>{
+        //         dispatch(uploadImage(url));
+        //     }).catch(err=>{
+        //         dispatch(uploading(false));
+        //     })
+        // })
+
+    }; //return
+};//uploadImageFB
+
+export default handleActions({
+    [UPLOAD_IMG] : (state,action)=> produce(state, (draft)=>{
+        draft.image_url = action.payload.image_url;
+        draft.uploading =false;
+    }),
+    [UPLOADING]: (state,action)=> produce(state, (draft)=>{
+        draft.uploading =action.payload.uploading;
+    }),
+    [SET_PRE]: (state,action)=> produce(state, (draft)=>{
+        draft.preview =action.payload.preview;
+    }),
+},initialState);
+
+const actionCreators = {
+    uploadImage,
+    uploadImageFB,
+    setPreview,
+}
+
+export {actionCreators};
