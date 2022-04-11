79a71816dee7679227bdf277cc83c60caf53f58b
ryurim0109 <dbfla5036@naver.com>
2022-04-11 10:28:18 +0900

    feat.conFigStore.js

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -3,11 +3,19 @@ import thunk from "redux-thunk";
 import { createBrowserHistory } from "history";
 import { connectRouter } from "connected-react-router";
 
+import User from "./modules/user";
+// import Post from "./modules/post";
+// import Comment from "./modules/comment";
+// import Image  from "./modules/image";
+
 
 export const history =createBrowserHistory();
 
 const rootReducer = combineReducers({
-    
+    user: User,
+    // post:Post,
+    // image:Image,
+    // comment: Comment,
     router:connectRouter(history),
   });
 
