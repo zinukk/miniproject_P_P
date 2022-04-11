60d0e9690da71be024ea7a8b22274dcbca119a1f
ryurim0109 <dbfla5036@naver.com>
2022-04-11 15:15:07 +0900

    feat.comment

--- src/redux/modules/comment.js
+++ src/redux/modules/comment.js
@@ -1,42 +1,42 @@
 import { createAction, handleActions } from "redux-actions";
 import {produce} from "immer";
-import "moment";
+
 import moment from "moment";
 
-// const ADD_COMMENT = "ADD_COMMENT";
-// const SET_COMMENT = "SET_COMMENT";
-// const DELETE_COMMENT = "DELETE_COMMENT"
+const ADD_COMMENT = "ADD_COMMENT";
+const SET_COMMENT = "SET_COMMENT";
+const DELETE_COMMENT = "DELETE_COMMENT"
 
-// const addComment = createAction(ADD_COMMENT, (comment_list) => ({comment_list}));
-// const setComment = createAction(SET_COMMENT, (comment_list)=>({comment_list}));
-// const deleteComment = createAction(DELETE_COMMENT, (commentId)=>({commentId}));
+const addComment = createAction(ADD_COMMENT, (comment_list) => ({comment_list}));
+const setComment = createAction(SET_COMMENT, (comment_list)=>({comment_list}));
+const deleteComment = createAction(DELETE_COMMENT, (commentId)=>({commentId}));
 
-// const initialState = {
-//     list:[],
-//   };
+const initialState = {
+    list:[],
+  };
 
-// const addCommentDB = (postId, content) =>{
-//     return function(dispatch, getState, {history}){
-//       const TOKEN = sessionStorage.getItem("token");
+const addCommentDB = (postId, content) =>{
+    return function(dispatch, getState, {history}){
+    //   const TOKEN = sessionStorage.getItem("token");
      
-//       instance.post(`/api/comments/${postId}`,{content},{ headers: {
-//           "authorization" : `${TOKEN}`
-//         }}).then((response)=>{
+    //   instance.post(`/api/comments/${postId}`,{content},{ headers: {
+    //       "authorization" : `${TOKEN}`
+    //     }}).then((response)=>{
          
-//             let comment_list = {...response.data};
+    //         let comment_list = {...response.data};
    
-//             dispatch(addComment(comment_list))
+    //         dispatch(addComment(comment_list))
             
-//       dispatch(postActions.newComment(parseInt(postId)));
-//       // 코멘트의 숫자를 셀때는 post정보에 포함된 comments 배열의 길이로 숫자를 세서 화면에 표현하므로 post 리덕스의 상태도 수정해주어야 한다.
+    //   dispatch(postActions.newComment(parseInt(postId)));
+    //   // 코멘트의 숫자를 셀때는 post정보에 포함된 comments 배열의 길이로 숫자를 세서 화면에 표현하므로 post 리덕스의 상태도 수정해주어야 한다.
 
-//     })
-// }
-// }
+    // })
+}
+}
 
 
-// const deleteCommentDB = (commentId) => {
-// return function (dispatch, getState, { history }) {
+const deleteCommentDB = (commentId) => {
+return function (dispatch, getState, { history }) {
 
 //   const TOKEN = sessionStorage.getItem("token");
 
@@ -53,11 +53,11 @@ import moment from "moment";
 //     .catch((err) => {
 //       console.log("withdraw : 에.러", err);
 //     });
-// };
-// };
+};
+};
 
-// const getComment = (postId) => {
-// return function (dispatch, getState, { history }) {
+const getComment = (postId) => {
+return function (dispatch, getState, { history }) {
 //   const TOKEN = sessionStorage.getItem("token");
 //   instance.get("/",{
 //     headers: {
@@ -72,39 +72,40 @@ import moment from "moment";
  
    
 //   });
-// }
-// }
-// export default handleActions(
-// {
-//   [ADD_COMMENT]: (state, action) => produce(state, (draft)=> {
+}
+}
+export default handleActions(
+{
+  [ADD_COMMENT]: (state, action) => produce(state, (draft)=> {
    
-//       draft.list.shift(action.payload.content);    //unshift 는 최신이 맨앞에, shift는 최신이 맨 뒤에 배치, 이렇게하면 댓글달자마자 디테일페이지 가도 에러없음
+    //   draft.list.shift(action.payload.content);    
+      //unshift 는 최신이 맨앞에, shift는 최신이 맨 뒤에 배치, 이렇게하면 댓글달자마자 디테일페이지 가도 에러없음
      
 
-//     }),
+    }),
 
-//     [SET_COMMENT]: (state, action) => produce(state, (draft)=> {
-//         draft.list = action.payload.comment_list;
+    [SET_COMMENT]: (state, action) => produce(state, (draft)=> {
+        // draft.list = action.payload.comment_list;
       
-//     }),
-//     [DELETE_COMMENT]: (state, action) => produce(state, (draft)=> {
-//       let new_comment_list = draft.list.filter((v) => {
-//         if(v.commentId !== action.payload.commentId){
-//           return v
-//         }
-//       })
-//       draft.list = new_comment_list;
-//   }),
+    }),
+    [DELETE_COMMENT]: (state, action) => produce(state, (draft)=> {
+    //   let new_comment_list = draft.list.filter((v) => {
+    //     if(v.commentId !== action.payload.commentId){
+    //       return v
+    //     }
+    //   })
+    //   draft.list = new_comment_list;
+  }),
     
-// }, initialState)
+}, initialState)
 
 
-// const actionCreators ={
-//   addComment,
-//   addCommentDB,
-//   getComment,
-//   setComment,
-//   deleteCommentDB,
-// }
+const actionCreators ={
+  addComment,
+  addCommentDB,
+  getComment,
+  setComment,
+  deleteCommentDB,
+}
 
-// export {actionCreators};
+export {actionCreators};
